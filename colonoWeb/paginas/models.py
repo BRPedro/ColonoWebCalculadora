423559232cc876c538b71bfd7da78c124673e517
Pedro Barrantes Rodríguez <barrantes.r.pedro@gmail.com>
2016-12-09 12:02:48 -0600

    AntesBD
    Antes de imprementar bd

--- colonoWeb/paginas/models.py
+++ colonoWeb/paginas/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class TC_PATRON(models.Model):
+    ID_PARONT =models.AutoField(primary_key=True)
+    FILA_P =   models.IntegerField(null=True)
+    COLUMNA_P = models.IntegerField(null=True)
+
+
+
+class TC_COORDENADA(models.Model):
+    ID_COORDENADAN= models.AutoField(primary_key=True)
+    FILA_C=models.IntegerField()
+    COLUMNA_C=models.IntegerField()
+    ID_TC_PATRON = models.ForeignKey(TC_PATRON,null=True)
